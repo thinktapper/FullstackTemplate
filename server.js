9056e91dee34b9adf93b8b64f7b1f9b431eee757
Andrew Tapper <andrew@tapper.codes>
2022-06-24 22:03:51 -0500

    figured it out, had to create server.js file before npm init 🙈

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'sample_mflix',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log(`Connected to Database`)
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port = ${PORT}`)
+})
