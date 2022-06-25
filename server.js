01bd8d0c570d8b2896b57da3aa1b35c60f615c93
Andrew Tapper <andrew@tapper.codes>
2022-06-24 22:24:11 -0500

    deployed to heroku (.env variable added manually to heroku 'config vars')

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ MongoClient.connect(dbConnectionString)
         collection = db.collection('movies')
     })
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port = ${PORT}`)
 })
